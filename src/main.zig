317b13e373d6f777b9799cb0aa06da78d59338e8
Alessandro Vezzoli <alessandrovezzoli1@gmail.com>
2026-01-24 11:20:00 +0100

    codecrafters submit [skip ci]

--- src/main.zig
+++ src/main.zig
@@ -1,21 +1,33 @@
 const std = @import("std");
+const Io = std.Io;
 
 var stdout_writer = std.fs.File.stdout().writerStreaming(&.{});
 const stdout = &stdout_writer.interface;
 
+var input_buffer: [4096]u8 = undefined;
+var reader = std.fs.File.stdin().readerStreaming(&input_buffer);
+const stdin = &reader.interface;
+
 pub fn main() !void {
-    // TODO: Uncomment the code below to pass the first stage
+    while (true) {
+        const command = try prompt_new_line();
+        try handle_command(command);
+    }
+}
+
+fn prompt_new_line() !?[]const u8 {
     try stdout.print("$ ", .{});
+    return try stdin.takeDelimiter('\n');
+}
 
-    var input_buffer: [4096]u8 = undefined;
-    var reader = std.fs.File.stdin().readerStreaming(&input_buffer);
-    const stdin = &reader.interface;
-
-    const command = try stdin.takeDelimiter('\n');
-
+fn handle_command(command: ?[]const u8) !void {
     if (command) |c| {
-        try stdout.print("{s}: command not found\n", .{c});
+        if (c.len == 0) {
+            try stdout.print("\n", .{});
+        } else {
+            try stdout.print("{s}: command not found\n", .{c});
+        }
     } else {
-        try stdout.print("EOF\n", .{});
+        try stdout.print("\n", .{});
     }
 }
