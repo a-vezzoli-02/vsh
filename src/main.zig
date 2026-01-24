568f841ae56d4601bd5e47391d60aa26aa9fb0e1
Alessandro Vezzoli <alessandrovezzoli1@gmail.com>
2026-01-24 11:11:12 +0100

    codecrafters submit [skip ci]

--- src/main.zig
+++ src/main.zig
@@ -6,4 +6,16 @@ const stdout = &stdout_writer.interface;
 pub fn main() !void {
     // TODO: Uncomment the code below to pass the first stage
     try stdout.print("$ ", .{});
+
+    var input_buffer: [4096]u8 = undefined;
+    var reader = std.fs.File.stdin().readerStreaming(&input_buffer);
+    const stdin = &reader.interface;
+
+    const command = try stdin.takeDelimiter('\n');
+
+    if (command) |c| {
+        try stdout.print("{s}: command not found\n", .{c});
+    } else {
+        try stdout.print("EOF\n", .{});
+    }
 }
